832052054992c417a3281680432dea3171c51bed
Victor-G87 <victor.g.hernandez87@gmail.com>
2021-09-29 19:49:08 -0500

    livesite

---
+++ .idea/js/particles.js
@@ -0,0 +1,16 @@
+      /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+      particlesJS.load('particles-js', 'particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+
+         /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+         particlesJS.load('particles-js2', 'particles.json', function() {
+            console.log('callback - particles.js config loaded');
+          });
+
+             /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+         particlesJS.load('particles-js3', 'particles2.json', function() {
+            console.log('callback - particles.js config loaded');
+          });
+
+
